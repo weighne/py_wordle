91ce3ca9be0031ac7def523469c53062ccb29afc
weighne <cvickery@posteo.net>
2022-01-18 18:47:05 -0500

    Cleaned up gameplay and working on color coded output

--- py_wordle.py
+++ py_wordle.py
@@ -1,5 +1,7 @@
 import random
 import sys
+from os import system, name
+from termcolor import colored
 
 # getting the word list in order
 # with open("words.txt") as wordlist:
@@ -19,12 +21,27 @@ import sys
 #             continue
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 wrong_letters = []
+user_guesses = []
+guess_guides = []
+
+def cls():
+    input('Press any key to continue...')
+    if name == 'nt':  # for windows
+        _ = system('cls')
+    else:  # for mac and linux(here, os.name is 'posix')
+        _ = system('clear')
 
 
-def print2(list):
-    for x in list:
+def print2(*text):
+    for x in text:
         sys.stdout.write(x + ' ')
     print()
+
+
+def print_blue(text):
+    output = colored(text, 'blue')
+    print2(output)
+    # print(colored(text, 'blue'))
 
 
 def get_word():
@@ -46,27 +63,40 @@ def set_array(word):
 
 
 def check_guess(user_input, word, blank):
+    letter_guide = []
+    user_guesses.append(user_input)
+    print2(user_input)
+
     # global wrong_letters
     for x in range(len(user_input)):
         if user_input[x] == word[x]:
-            print(f"Letter {x+1} Match!")
+            # print(f"Letter {x+1} Match!")
+            sys.stdout.write(user_input[x] + ' ')
+            letter_guide.append(user_input[x])
             blank[x] = user_input[x]
             for y in range(len(alphabet)):
                 if alphabet[y] == user_input[x]:
                     alphabet[y] = f'{user_input[x]}'
         elif user_input[x] in word:
-            print(f"Letter {x+1} Wrong spot, right letter!")
+            # print(f"Letter {x+1} Wrong spot, right letter!")
+            sys.stdout.write('? ')
+            letter_guide.append('?')
             for y in range(len(alphabet)):
                 if alphabet[y] == user_input[x]:
                     alphabet[y] = f'({user_input[x]})'
         else:
-            print(f"Letter {x+1} Wrong letter entirely!")
+            # print(f"Letter {x+1} Wrong letter entirely!")
+            sys.stdout.write('X ')
+            letter_guide.append('X')
             for y in range(len(alphabet)-1):
                 if alphabet[y] == user_input[x]:
                     alphabet.pop(y)
                 else:
                     continue
-            wrong_letters.append(user_input[x])
+            if user_input[x] not in wrong_letters:
+                wrong_letters.append(user_input[x])
+
+    guess_guides.append(''.join(letter_guide))
 
 def get_input():
     user_input = input("Guess a 5 letter word: ")
@@ -78,17 +108,27 @@ if __name__ == '__main__':
     # print(game_word)
     blank = set_array(game_word)
     while True:
-        print('Remaining Letters:')
+        cls()
+        if len(user_guesses) >= 1:
+            for x in range(len(user_guesses)):
+                print2(user_guesses[x])
+                print2(print_blue(guess_guides[x]))
+
+        print('\nRemaining Letters:')
         print2(alphabet)
         print('Wrong Letters:')
         print2(wrong_letters)
-        print2(blank)
+        print_blue(blank)
 
         if ''.join(blank) == game_word:
             print(f"You Won!\nThe word is: {game_word}")
+            for x in range(len(user_guesses)-1):
+                user_guesses.pop(x)
             break
         elif guesses >= 6 and ''.join(blank) != game_word:
             print(f"You Lose!\nThe word was: {game_word}")
+            for x in range(len(user_guesses)-1):
+                user_guesses.pop(x)
             break
         else:
             user_input = get_input()
